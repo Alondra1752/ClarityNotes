602dedb5bd1afd2de29dc80e8735ee71b29589c0
Alondra1752 <alondrasant7739@gmail.com>
2024-09-05 13:40:05 -0500

    added utilities folder and fixed routes in html

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -181,6 +181,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
+
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
